5a0b55494cbe17b7ec7270276e793d7db94b17cc
Albores López Jorge Daniel <daknst@gmail.com>
2018-05-10 18:26:14 -0500

    Probando a ver si me deja subir la vaina

--- app/src/main/java/com/example/fiuady/chatapp/MainActivity.java
+++ app/src/main/java/com/example/fiuady/chatapp/MainActivity.java
@@ -34,6 +34,7 @@ class User {
     public User(int id, String firstName, String lastName, int phoneNumber) {
         this.id = id;
         this.firstName = firstName;
+
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
     }
